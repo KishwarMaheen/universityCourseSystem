bbd54830bd16ba5d18f3b1408ba66af5f3ad8846
Kishwar Maheen <kishwar.maheen@gmail.com>
2023-12-03 16:10:02 +0600

    gorm installation and course models

---
+++ models/courseModels.go
@@ -0,0 +1,30 @@
+package models
+
+import "gorm.io/gorm"
+
+type Department struct {
+	// Department model
+	gorm.Model
+	Name string
+}
+
+type Course struct {
+	// Course model
+	gorm.Model
+	Name           string
+	DepartmentID   int
+	Department     Department
+	PrerequisiteID int
+	Prerequisite   *Course
+	credits        int
+}
+
+type Program struct {
+	// Program model - for example Masters in something
+	gorm.Model
+	Name         string
+	Courses      []Course `gorm:"many2many:program_courses;"`
+	DepartmentID int
+	Department   Department
+	TotalCredits int
+}
